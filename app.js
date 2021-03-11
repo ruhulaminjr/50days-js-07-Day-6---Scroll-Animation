9649c5544bd637d31782475cf2124494893eb9ca
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2021-03-11 17:36:12 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll',checkboxes)
+checkboxes();
+
+function checkboxes(){
+    const triggerbottm = window.innerHeight / 5 * 4 ;
+
+    // console.log(triggerbottm);
+    // console.log(window.innerHeight);
+
+    boxes.forEach(box => {
+        const boxtop = box.getBoundingClientRect().top;
+        if(boxtop < triggerbottm){
+            box.classList.add('show');
+        }else{
+            box.classList.remove('show');
+        }
+    })
+}
+
